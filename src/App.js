15c858279191fba95c5b142a9a219337ac35b50f
Sourav Pal <143326384+Souravpal08@users.noreply.github.com>
2024-04-09 22:00:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Tictactoe from './components/TicTacToe/Tictactoe';
+
+function App() {
+  return (
+    <div >
+      <Tictactoe />
+    </div>
+  );
+}
+
+export default App;
